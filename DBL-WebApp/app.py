82860ab63b2cdd449cd68b479d6dcae8edd33724
rdvhoorn <r.v.hoorn99@gmail.com>
2018-05-02 15:01:33 +0200

    Uploadbutton
    Uploadbutton without layout

--- DBL-WebApp/app.py
+++ DBL-WebApp/app.py
@@ -1,12 +1,34 @@
-from flask import Flask
+
+import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def index():
+    return render_template("upload.html")
+
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    target = os.path.join(APP_ROOT, "trees/")
+    print(target)
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist("file"):
+        print(file)
+        filename = file.filename
+        destination = "/".join([target, filename])
+        print(destination)
+        file.save(destination)
+
+    return render_template("complete.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
