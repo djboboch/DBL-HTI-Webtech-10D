96ef83573c3d61541f12ff61c55b5bbdf662c175
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2018-06-13 14:24:01 +0200

    Update convertToJson.py

--- pyscripts/convertToJson.py
+++ pyscripts/convertToJson.py
@@ -14,15 +14,25 @@ tree_name = os.path.splitext(os.path.basename("uploads/" + file_name))[0]
 
 def processing():
     tree = Tree(pathToTree, format=1) #Creates the ete3 tree object
-    jsongraph = newicktojson(tree, tree) #invokes newicktojson using the tree object
+    jsongraph = newicktojsonInitialize(tree, tree) #invokes newicktojson using the tree object
     with open(tree_name + '.json', 'a') as the_file: #opens the json file
         the_file.write(jsongraph) #saves the content of jsongraph into the json file
     shutil.move(tree_name + '.json', "convertedTrees/" + tree_name + ".json" ) #moves the tree into correct folder
     return
 
+i = 0
+def newicktojsonInitialize(tree, nodeG):
+    global i
+    i = 0
+    treestringjson = newicktojson(tree, nodeG)
+    return treestringjson
+
 def newicktojson(tree, nodeG):
+    global i
+    i = i + 1
     treestring = ""
     treestring += "{ \n\"name\": \"" + nodeG.name + "\""
+    treestring += ",\n\"nodeId\": \"" + str(i) + "\""
     if nodeG.children:
         treestring += ",\n\"children\": ["
         list = []
@@ -35,7 +45,7 @@ def newicktojson(tree, nodeG):
         if hasattr(nodeG, 'size'):
             treestring += nodeG.size
         else:
-            treestring += str(randint(1000, 10000))
+            treestring += str(1000)
     treestring += "}"
     return treestring
 
