2e2a6d4e3f5877c6c957367dbcc598d30fa5d789
Yoon <y.yoon@student.tue.nl>
2018-06-21 19:55:38 +0200

    some changes

--- public/javascripts/popup.js
+++ public/javascripts/popup.js
@@ -12,12 +12,12 @@ spn.onclick = function() {
     mod.style.display = "none"
 };
 
-function doOnce() {
-    if (document.cookie.replace(/(?:(?:^|.*;\s*)doSomethingOnlyOnce\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
+function showOnce() {
+    if (document.cookie.replace(/(?:(?:^|.*;\s*)Once\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
         $("#explain_pop").delay(200).fadeIn();
         var date = new Date();
         var time = new Date(date.getTime() + 86400000); //expires after a day
-        document.cookie = "doSomethingOnlyOnce=true; expires=time.toUTCString();";
+        document.cookie = "Once=true; expires=time.toUTCString();";
     }
     $('#close_1, #explain_pop').click(function(){
         $("#explain_pop").fadeOut();
